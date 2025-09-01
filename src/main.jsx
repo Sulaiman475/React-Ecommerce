c8bdef6f9c428e3443b02f9f26a3a102f31175e2
Muhammad Sulaiman <131637548+Sulaiman475@users.noreply.github.com>
2025-09-01 16:15:45 +0500

    Github dev Testing

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import { store } from './redux/store.jsx'
+// import { store } from './redux/store.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
